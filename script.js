f226dd21d1a9d48ef7485a22b81edb4b066f4aab
George Gordon <georgegordon5@gmail.com>
2025-03-17 13:00:58 +0000

    Extention under construciton with animation has been added

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
   menu.classList.toggle('open');
   icon.classList.toggle('open');
 }
+
+
+
+        // GSAP Animation
+        gsap.fromTo(".message span", 
+            { y: 50, opacity: 0 },
+            { 
+              y: 0, 
+              opacity: 1, 
+              duration: 3,
+              ease: "back.out(5.7)", 
+              stagger: 0.5,
+              repeat: -1,             // Infinite repetitions
+              yoyo: true,             // Animates forward then reverses
+              repeatDelay: 1          // Optional pause between repetitions
+            }
+        );
+  
